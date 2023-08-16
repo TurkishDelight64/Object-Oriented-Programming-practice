77c9d6f78b0734dbd454cc191b8422540f5a349c
Ahmed Said Yazici <115704730+TurkishDelight64@users.noreply.github.com>
2023-08-16 21:03:34 +0100

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesPractice
+{
+    internal class Employee
+    {
+
+        // fields
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        //Constructor if only given 4 values 
+        public Employee(string First, string Last, string em, DateTime bd) : this(First, Last, em, bd, 0)
+        {
+
+        }
+
+        //Default constructor
+        public Employee(string first, string Last, string em, DateTime bd, double rate) 
+        {
+            firstName = first;
+            lastName = Last;
+            email = em;
+            birthDay = bd;
+            hourlyRate = rate;
+        }
+
+        // Method that adds to the numberOfHoursWorked and prints it
+        public void PerformWork()
+        {
+            //Initailly this method just added one to numberOfHoursWorked but I updated it to call PerformWork give it 1 as a paramtrer.
+            //numberOfHoursWorked++;
+            PerformWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hours!");
+        }
+
+        // Methods overloading 
+        // Method that adds to the numberOfHoursWorked and prints it but, you get to pick how many hours is worked
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+
+        //Calculates wages and prints them
+        public double RecieveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
+
+            if (resetHours)
+                numberOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        //Prints the name, lastname, email, and birthday of employees
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+using ClassesPractice;
+
+Console.WriteLine("Making an Employee");
+Console.WriteLine("------------------\n");
+
+//Making a new object "bethany" of type Employee by invoking the Employee class. It also uses the constructor, pretty cool :)
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+//Displays bethanies details
+bethany.DisplayEmployeeDetails();
+
+//Bethany goes to work
+for(int i = 1; i < 5; i++)
+{
+    bethany.PerformWork(i);
+}
+
+bethany.PerformWork(5);
+
+//Calculates Bathenies wage
+double receiveWageBethany = bethany.RecieveWage(true);
+Console.WriteLine($"Wage was paid (message from program): {receiveWageBethany}");
+
+//Changing the values of bethany
+bethany.firstName = "John";
+bethany.lastName = "WIlliam";
+
+bethany.DisplayEmployeeDetails();
+
+//New employee
+Console.WriteLine("Making an Employee");
+Console.WriteLine("------------------\n");
+
+//New object, George
+Employee george = new("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+
+//Georges details
+george.DisplayEmployeeDetails();
+
+
+//George goes to work
+for (int i = 1; i < 5; i++)
+{
+    george.PerformWork(i);
+}
+
+george.PerformWork(5);
+
+var receivedWageGeorge = george.RecieveWage(true);
